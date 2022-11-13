ce474fe76d62aa6538a3448030c9e0727982e51b
Mario Aguilar <mariocba150@gmail.com>
2022-11-13 12:53:17 -0300

    Finalizo el Contador de la Actividad-Workbook-1

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,18 @@
 import './ItemListContainer.css';
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = (props) => {
+	//Función que será usada por el contador
+	const onAdd = (cantidad) => {
+		console.log(cantidad);
+	};
 	return (
-		<div className="greeting-container">
-			<h2 className="greeting-message">{props.greeting}</h2>
-		</div>
+		<>
+			<div className="greeting-container">
+				<h2 className="greeting-message">{props.greeting}</h2>
+			</div>
+			<ItemCount stock={5} initial={1} onAdd={onAdd} />
+		</>
 	);
 };
 
