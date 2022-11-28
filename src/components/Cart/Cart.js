51fc00bf91b1323bece901864107825057f6649c
Mario Aguilar <mariocba150@gmail.com>
2022-11-28 19:46:02 -0300

    inicio actividad

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,40 @@
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext/CartContext';
 import ItemCart from '../ItemCart/ItemCart';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+
 import './Cart.css';
 
 const Cart = () => {
 	const { cartList, totalPrice } = useCartContext();
+
+	const order = {
+		buyer: {
+			user: 'Luciano_Lopez',
+			email: 'lucianolopez@gmail.com',
+			phone: '3515111111',
+			address: 'San Martin 1024',
+		},
+		item: cartList.map((product) => ({
+			id: product.id,
+			title: product.title,
+			price: product.price,
+			quantity: product.quantity,
+		})),
+		total: totalPrice(),
+	};
+
+	const buyFinish = () => {
+		const buyDb = getFirestore();
+		const orderBuyCollection = collection(buyDb, 'orders');
+		addDoc(orderBuyCollection, order)
+			.then(({ id }) => {
+				console.log(id);
+				alert(`Su orden de compra ha sido generada con el nº ${id}`);
+			})
+			.catch((error) => console.log(error));
+	};
+
 	if (cartList.length === 0) {
 		return (
 			<>
@@ -17,13 +47,19 @@ const Cart = () => {
 	}
 
 	return (
-		<div className="cartContainer">
-			{cartList.map((product) => (
-				<ItemCart key={product.id} product={product} />
-			))}
-
-			<h1 className="totalPrice">Precio Total: ${totalPrice()}</h1>
-		</div>
+		<>
+			<div className="cartContainer">
+				{cartList.map((product) => (
+					<ItemCart key={product.id} product={product} />
+				))}
+			</div>
+			<div>
+				<h1 className="totalPrice">Precio Total: ${totalPrice()}</h1>
+				<button className="buttonBuy" onClick={buyFinish}>
+					Comprar
+				</button>
+			</div>
+		</>
 	);
 };
 
